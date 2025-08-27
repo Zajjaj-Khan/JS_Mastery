606b0aba097ef3ce19443e61c410f61044c0f02b
Zajjaj Bin Sana <kzajjaj@gmail.com>
2025-08-27 09:21:35 +0500

    Functional Programming Commit

---
+++ Javascript-lesson/Functional_Programming/FirstClassPrograming/FirstClassFunction.js
@@ -0,0 +1,20 @@
+//Functions can be Treated like can be any other value
+
+
+
+const greet= function (person){
+    console.log( `Hello, ${person}!`);
+}
+ function Greet(name) {
+        console.log(`Hi, ${name}!`);
+    }
+function Hate(name) {
+        console.log(`Hey I hate, ${name}!`);
+    }
+
+
+[1,2,3].map(function (num) {})
+
+callWithBlue((color)=>{
+    console.log(`The color is ${color}`);
+})
